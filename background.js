6a0af398b1fc58aac611b3f97e191416eb1de56e
SealJackNote <159676756+SealJackNote@users.noreply.github.com>
2024-02-12 00:02:07 -0500

    Create background.js

---
+++ background.js
@@ -0,0 +1,20 @@
+// temp code taken from https://github.com/avirads/sidepanel_mvp/tree/main
+function setupContextMenu(){
+	chrome.contextMenus.create({
+		id: "test",
+		title: "test",
+		contexts: ["selection"]
+	})
+}
+
+chrome.runtime.onInstalled.addListener(()=>{
+	setupContextMenu()
+})
+
+chrome.runtime.onStartup.addListener(()=>{
+	setupContextMenu()
+	chrome.storage.sync.get(["savedNotes"]).then((result) => {
+		for (i = 0; i < result.savedNotes.length; i++) {
+			container.appendChild(result.savedNotes[i])
+	}
+})

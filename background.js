1374a59bace8989cabf654c1f10d3c7886de510b
Nat <patheticmustan@gmail.com>
2024-05-24 04:40:55 -0400

    open panel on icon click

--- background.js
+++ background.js
@@ -19,3 +19,12 @@ chrome.runtime.onStartup.addListener(()=>{
 		}
 	})
 })
+
+chrome.commands.onCommand.addListener((command) => {
+	console.log(`Command "${command}" triggered`)
+})
+
+// open panel onclick
+chrome.sidePanel
+	.setPanelBehavior({ openPanelOnActionClick: true })
+	.catch((error) => console.error(error));

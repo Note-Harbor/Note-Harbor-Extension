56dcd897caab314bd2d12b8e82335e3dc9fe9181
Nat <patheticmustan@gmail.com>
2024-05-26 22:30:18 -0400

    broken code in serviceworker....

--- background.js
+++ background.js
@@ -1,25 +1,3 @@
-// temp code taken from https://github.com/avirads/sidepanel_mvp/tree/main
-function setupContextMenu(){
-	chrome.contextMenus.create({
-		id: "test",
-		title: "test",
-		contexts: ["selection"]
-	})
-}
-
-chrome.runtime.onInstalled.addListener(()=>{
-	setupContextMenu()
-})
-
-chrome.runtime.onStartup.addListener(()=>{
-	setupContextMenu()
-	chrome.storage.sync.get(["savedNotes"]).then((result) => {
-		for (i = 0; i < result.savedNotes.length; i++) {
-			container.appendChild(result.savedNotes[i])
-		}
-	})
-})
-
 chrome.commands.onCommand.addListener((command) => {
 	console.log(`Command "${command}" triggered`)
 })

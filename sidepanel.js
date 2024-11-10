c42bb2230bb47d633bf0961ea087a86e810765d7
Nat <patheticmustan@gmail.com>
2024-11-09 23:54:10 -0500

    italics and underline in markdown bar

--- sidepanel.js
+++ sidepanel.js
@@ -385,13 +385,14 @@ function createFormatBar() {
   const bottomBar = document.createElement("div");
   bottomBar.className = "bottom-bar";
 
+  // only run if the current active element is note-content (add info box?)
+  const classWhitelist = ["note-content"];
+
   const bold = document.createElement("button");
   bold.textContent = "B";
   bold.style.fontWeight = "bold";
   bold.addEventListener("pointerdown", evt => {
     evt.preventDefault();
-    // only run if the current active element is either note-title or note-content
-    const classWhitelist = ["note-title", "note-content"];
     const element = document.activeElement;
     const classesOfSelectedElement = element.classList.value;
     if (classWhitelist.some(v => classesOfSelectedElement.indexOf(v) !== -1)) {
@@ -413,7 +414,22 @@ function createFormatBar() {
   italic.style.fontStyle = "italic";
   italic.addEventListener("pointerdown", evt => {
     evt.preventDefault();
-    document.execCommand("italic");
+    const element = document.activeElement;
+    const classesOfSelectedElement = element.classList.value;
+    if (classWhitelist.some(v => classesOfSelectedElement.indexOf(v) !== -1)) {
+      const selectedText = element.value.substring(element.selectionStart, element.selectionEnd);
+
+      if (element.nodeName === "TEXTAREA") {
+        // if the entire selection is italics...
+        // SUBTLE: AVOID MATCHING BOLD!!!!
+        // TODO: fix this, this is still not totally functional when you have a weird amount of asterisks
+        if (selectedText.match(/^(?:\*\*)*\*[^\*].+|.+[^\*]\*(?:\*\*)*$/)) {
+          element.setRangeText(selectedText.substring(1, selectedText.length-1));
+        } else {
+          element.setRangeText(`*${selectedText}*`);
+        }
+      }
+    }
   });
 
   const underline = document.createElement("button");
@@ -421,7 +437,20 @@ function createFormatBar() {
   underline.style.textDecoration = "underline";
   underline.addEventListener("pointerdown", evt => {
     evt.preventDefault();
-    document.execCommand("underline");
+    const element = document.activeElement;
+    const classesOfSelectedElement = element.classList.value;
+    if (classWhitelist.some(v => classesOfSelectedElement.indexOf(v) !== -1)) {
+      const selectedText = element.value.substring(element.selectionStart, element.selectionEnd);
+
+      if (element.nodeName === "TEXTAREA") {
+        // if the entire selection is underlined... 
+        if (selectedText.match(/^<ins>.+<\/ins>$/)) {
+          element.setRangeText(selectedText.substring(5, selectedText.length-6));
+        } else {
+          element.setRangeText(`<ins>${selectedText}</ins>`);
+        }
+      }
+    }
   });
 
   bottomBar.appendChild(bold);

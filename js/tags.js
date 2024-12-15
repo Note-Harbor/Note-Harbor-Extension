dbc865a89d19f8c10507861b3d7a5d59ac6c6740
KevinZhang100 <113929224+KevinZhang100@users.noreply.github.com>
2024-12-15 16:25:24 -0700

    fix editable issues with tag and delete

--- js/tags.js
+++ js/tags.js
@@ -4,52 +4,64 @@ const addTag = document.getElementById("addTagButton");
 addTag.addEventListener("click", insertTag);
 
 function insertTag() {
+    // tag as wrapper, tag-input for inputs, del-tag for delete button to avoid any editable issues
     const tag = document.createElement("div");
     tag.className = "new-tag";
-    tag.contentEditable = true;
-    tag.textContent = "";
 
-    tag.addEventListener("keydown", function(event) {
+    const tagInput = document.createElement("div");
+    tagInput.className = "tag-input";
+    tagInput.contentEditable = true;
+    tagInput.textContent = "";
+
+    tagInput.addEventListener("focus", function() {
+        if (tagInput.textContent.trim() === "") {
+            tagInput.textContent = "";
+        }
+        tag.className = "tag";
+        tagInput.style.marginRight = "7px";
+        deleteButton.style.display = "block";
+    });
+
+    tagInput.addEventListener("blur", function() {
+        if (tagInput.textContent.trim() === "") {
+            tagInput.textContent = ""; 
+            tag.className = "new-tag"; // reset to new-tag if text is empty
+        }
+        tagInput.style.marginRight = "0px";
+        deleteButton.style.display = "none";
+    });
+
+    tagInput.addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
-            event.preventDefault();  // prevent adding newline 
+            event.preventDefault(); //prevent new-line
         }
     });
 
-    tag.addEventListener("input", function() {
-        const text = tag.innerText.trim(); 
-        if (text !== "") {
-            if (tag.className !== "tag") {
-                tag.className = "tag";  
-            }
+    tagInput.addEventListener("input", function() {
+        if (tagInput.textContent.trim() !== "") {
+            tag.className = "tag";
         } else {
-            if (tag.className !== "new-tag") {
-                tag.className = "new-tag";  
-            }
+            tag.className = "new-tag";
         }
     });
 
     const deleteButton = document.createElement("button");
-    deleteButton.className = "del";
+    deleteButton.className = "del-tag";
     deleteButton.textContent = "x";
-    // deleteButton.contentEditable = false;
-    deleteButton.style.display = "none";
     deleteButton.addEventListener("click", function(event) {
         event.stopPropagation();
         tag.remove();
     });
 
-    tag.addEventListener("mouseover", function() {
-        deleteButton.style.display = "block";
+    deleteButton.addEventListener("mousedown", function(event) {
+        event.preventDefault(); // prevent blur on click
     });
 
-    tag.addEventListener("mouseout", function() {
-        deleteButton.style.display = "none";
-    });
-
+    tag.appendChild(tagInput);
     tag.appendChild(deleteButton);
 
     tagContainer.appendChild(tag);
+
+    // automatically focus on new tag input
+    tagInput.focus();
 }
-
-
-

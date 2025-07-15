6ad33e59be03733b55068f4dde4afbcc8e82e1f0
Jaclenga <jack3lenga@gmail.com>
2025-07-14 20:21:13 -0400

    Fix format bar alignment

--- js/formatBar.js
+++ js/formatBar.js
@@ -5,18 +5,18 @@
 function createFormatBar() {
     const bottomBar = document.createElement("div");
     bottomBar.className = "bottom-bar";
+    bottomBar.style.justifyContent = "center";
+    bottomBar.style.alignItems = "center";
+    bottomBar.style.display = "flex";
+    bottomBar.style.width = "fit-content";
+    bottomBar.style.padding = "4px 0";
 
     bottomBar.innerHTML = `
-  <span class="ql-formats">
-    <button class="ql-bold"></button>
-    <button class="ql-italic"></button>
-    <button class="ql-underline"></button>
-    <button class="ql-strike"></button>
-  </span>
-  <span class="ql-formats">
-    <button class="ql-script" value="sub"></button>
-    <button class="ql-script" value="super"></button>
+  <span class="ql-formats" style="display: flex; gap: 2px; justify-content: center; margin: 0 auto;">
+    <button class="ql-bold">B</button>
+    <button class="ql-italic">I</button>
+    <button class="ql-underline">U</button>
+    <button class="ql-strike">S</button>
   </span>`;
-
     return bottomBar;
 }
